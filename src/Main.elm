00e6149967229986cbb7fe9d68e8ab4e7aada3b9
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-11-08 23:04:51 +0900

    :+1:

--- src/Main.elm
+++ src/Main.elm
@@ -4,24 +4,8 @@ import Html exposing (text)
 
 
 main =
-    text (Debug.toString meaningOfLife)
+    text (bottlesOf "" 3)
 
-
-greeting : String
-greeting =
-    "Hello, Static Elm!"
-
-
-meaningOfLife : Int
-meaningOfLife =
-    42
-
-
-pi : Float
-pi =
-    3.14
-
-
-canChunk : Bool
-canChunk =
-    True
+bottlesOf : String -> Int -> String
+bottlesOf contents amount =
+    String.fromInt amount ++ " bottles of " ++ contents ++ "on the wall."
